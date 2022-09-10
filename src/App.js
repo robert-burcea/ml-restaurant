865dbb014d2b3f64aa42aa84869bf2ca721c3866
rburcea <robert.burcea@gmail.com>
2022-09-10 15:15:35 +0300

    algo works

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
-import React from 'react'
-import GetData from './GetData';
-import SimpleRegression from './SimpleRegression'
-import {useData, useSetData} from './DataContext'
+import React, {useEffect, useState} from 'react'
+import SimpleLinearRegression from 'ml-regression-simple-linear';
 
-const DataContext = React.createContext({x:[], y:[]});
+
 
 function App() {
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+  const [result, setResult] = useState();
+  const [ready, setReady] = useState(false);
+
+  const ml = () => {
+    const a = x.split(',').map((item) => { return parseInt(item) });
+    const b = y.split(',').map((item) => { return parseInt(item) });
+    console.log(a,b)
+    const regression = new SimpleLinearRegression(a, b);
+    const json = regression.toJSON();
+    console.log(json)
+    const loaded = SimpleLinearRegression.load(json);
+    console.log('loaded', loaded)
+    console.log(Math.floor(loaded.predict(850)))
+    setResult(Math.floor(loaded.predict(850)))
+    setReady(true);
+    return Math.floor(loaded.predict(850));
+}
+
+
   return (
-    <DataContext.Provider value={{x:[], y:[]}}>
+ 
 
     <div className="mx-auto max-w-[1040px] bg-gray-400 h-screen border border-stone-500 text-center">
-      <SimpleRegression />
-      <GetData />
+      <div>
+        <div>{ready? result : <div>loading</div>}</div>
+      </div>
+      <div className="mx-auto max-w-[840px] flex flex-col">
+        <div>
+            <p className="text-xl font-bold p-4">Please insert X:</p>
+            <input className="shadow-xl rounded h-[40px]" onChange={(e) => {setX(e.target.value)}}></input>
+        </div>
+        <div>
+            <p className="text-xl font-bold p-4">Please insert Y:</p>
+            <input className="shadow-xl rounded h-[40px]" onChange={(e) => {setY(e.target.value)}}></input>
+        </div>
+        <button className="mx-auto m-5 p-2 w-min bg-slate-50 rounded shadow-xl hover:scale-110" onClick={() => {ml()}}>SUBMIT</button>
+        
+    </div>
     </div>
 
-    </DataContext.Provider>
+
   );
 }
 
