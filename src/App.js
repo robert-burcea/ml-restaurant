496c2456ec904d6598311e47fa683d173cba8371
rburcea <robert.burcea@gmail.com>
2022-09-12 22:19:15 +0300

    added algo for soups and others

--- src/App.js
+++ src/App.js
@@ -12,8 +12,15 @@ import 'tippy.js/themes/material.css';
 
 function App() {
   const [cx, setCX] = useState();
+  const [cx_sec, setCX_SEC] = useState();
   const [dish, setDish] = useState();
   const [earnings, setEarnings] = useState();
+  const [soup, setSoup] = useState();
+  const [garnish, setGarnish] = useState();
+  const [salad, setSalad] = useState();
+  const [desert, setDesert] = useState();
+  const [bread, setBread] = useState();
+
   const [loadedDish, setLoadedDish] = useState();
   const [loadedCash, setLoadedCash] = useState();
   const [num, setNum] = useState();
@@ -28,10 +35,17 @@ function App() {
     onSnapshot(colRef, (snapshot) => {
         snapshot.docs.forEach((doc) => {
           setCX(doc.data().cx)
-          setDish(doc.data().dish)
           setEarnings(doc.data().earnings)
+          setDish(doc.data().dish)
+          setCX_SEC(doc.data().cx_sec) 
+          setSoup(doc.data().soup)
+          setGarnish(doc.data().garnish)
+          setSalad(doc.data().salad)
+          setDesert(doc.data().desert)
+          setBread(doc.data().bread)
         })
     })
+
     setFetched(true)
   }
 
@@ -44,19 +58,54 @@ function App() {
     })
 
   const ml = () => {
-    const a = cx.split(' ').map((item) => { return parseInt(item) });
-    const b = dish.split(' ').map((item) => { return parseInt(item) });
-    const c = earnings.split(' ').map((item) => { return parseInt(item) });
+    const cx_modified = cx.split(' ').map((item) => { return parseInt(item) });
+    const earnings_modified = earnings.split(' ').map((item) => { return parseInt(item) });
+    const dish_modified = dish.split(' ').map((item) => { return parseInt(item) });
+    const cx_sec_modified = cx_sec.split(' ').map((item) => { return parseInt(item) });
+    const soup_modified = soup.split(' ').map((item) => { return parseInt(item) });
+    const garnish_modified = garnish.split(' ').map((item) => { return parseInt(item) });
+    const salad_modified = salad.split(' ').map((item) => { return parseInt(item) });
+    const desert_modified = desert.split(' ').map((item) => { return parseInt(item) });
+    const bread_modified = bread.split(' ').map((item) => { return parseInt(item) });
+
     const numberToCheck = parseInt(num);
-     const regressionDish = new SimpleLinearRegression(a, b);
-    const regressionCash = new SimpleLinearRegression(a,c)
+
+    const regressionDish = new SimpleLinearRegression(cx_modified, dish_modified);
+    const regressionCash = new SimpleLinearRegression(cx_modified,earnings_modified);
+    const regressionSoup = new SimpleLinearRegression(cx_sec_modified, soup_modified);
+    const regressionGarnish = new SimpleLinearRegression(cx_sec_modified,garnish_modified);
+    const regressionSalad = new SimpleLinearRegression(cx_sec_modified, salad_modified);
+    const regressionDesert = new SimpleLinearRegression(cx_sec_modified,desert_modified);
+    const regressionBread = new SimpleLinearRegression(cx_sec_modified, bread_modified);
+
+
     const jsonDish = regressionDish.toJSON();
     const jsonCash = regressionCash.toJSON();
+    const jsonSoup = regressionSoup.toJSON();
+    const jsonGarnish = regressionGarnish.toJSON();
+    const jsonSalad = regressionSalad.toJSON();
+    const jsonDesert = regressionDesert.toJSON();
+    const jsonBread = regressionBread.toJSON();
+
     const loadedDish = SimpleLinearRegression.load(jsonDish);
     const loadedCash = SimpleLinearRegression.load(jsonCash);
-    localStorage.setItem('loadedDish',loadedDish);
-    localStorage.setItem('loadedCash',loadedCash);    
-    setResult({dishes:Math.floor(loadedDish.predict(numberToCheck)), cash:Math.floor(loadedCash.predict(numberToCheck))})
+    const loadedSoup = SimpleLinearRegression.load(jsonSoup);
+    const loadedGarnish = SimpleLinearRegression.load(jsonGarnish);
+    const loadedSalad = SimpleLinearRegression.load(jsonSalad);
+    const loadedDesert = SimpleLinearRegression.load(jsonDesert);
+    const loadedBread = SimpleLinearRegression.load(jsonBread);
+  
+
+    setResult({
+      dishes:Math.floor(loadedDish.predict(numberToCheck)), 
+      cash:Math.floor(loadedCash.predict(numberToCheck)),
+      soup:Math.floor(loadedSoup.predict(numberToCheck)),
+      garnish:Math.floor(loadedGarnish.predict(numberToCheck)),
+      salad:Math.floor(loadedSalad.predict(numberToCheck)),
+      desert:Math.floor(loadedDesert.predict(numberToCheck)),
+      bread:Math.floor(loadedBread.predict(numberToCheck)),
+    })
+
     setReady(true);
 }
 
@@ -67,18 +116,61 @@ function App() {
 
       <div className="mx-auto m-3 p-2 boder shadow-sm bg-white w-fit rounded hover:cursor-pointer hover:scale-105 tippy"><span>Info</span></div><h1 className="text-2xl font-bold mb-3 tracking-widest">Eurest PEC101 ML ALGO</h1>
       <div className="mx-auto max-w-[840px] flex flex-col p-2">
-        <div className="flex flex-col border-b shadow-xl bg-[#CDE7BE]">
-            <p className="text-xl font-bold p-4">Introduceti numarul de clienti estimati:</p>
-            <input className="mx-auto text-center shadow-xl rounded h-[50px] m-2" onChange={(e) => {setNum(e.target.value)}}></input>
+        <div className="flex flex-col border-b shadow-xl bg-[#CDE7BE] m-2 rounded-xl">
+            <p className="text-xl font-bold p-4">Introduceti numarul estimat de clienti:</p>
+            <input className="mx-auto text-center text-lg shadow-xl rounded h-[50px] m-2" onChange={(e) => {setNum(e.target.value)}}></input>
             <button className="mx-auto m-5 p-2 w-min bg-slate-50 rounded shadow-xl hover:scale-110" onClick={() => {ml()}}>SUBMIT</button>
         </div>
 
-        <div className="max-w-[840px] border rounded shadow-xl m-3 bg-[#CDE7BE]">
-          <div className="mx-auto text-xl flex flex-col sm:flex-row justify-center items-center p-2"><p>Vor fi cumparate </p>{ready? <p className="text-3xl p-2 font-bold text-[#2E5EAA]">~{result.dishes}</p> : 
-          <div className="text-sm text-slate-600">...awaiting...</div>} <p>portii de fel principal</p></div>
-          <div className="mx-auto text-xl flex flex-col sm:flex-row justify-center items-center p-2"><p>Incasarile vor fi de </p>{ready? <p className="text-3xl p-2 font-bold text-[#2E5EAA]">~{result.cash}</p> : 
-          <div className="text-sm text-slate-600">...awaiting...</div>} <p>RON fara TVA</p></div>
-      </div>
+     
+          <div className="overflow-x-auto relative shadow-md rounded-lg">
+       <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400 rounded-lg">
+         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+         <tr>
+           <th scope="col" className="py-3 px-6">Categorie</th>
+           <th scope="col" className="py-3 px-6">Cantitate</th>
+           <th scope="col" className="py-3 px-6">UNITATE</th>
+         </tr>
+         </thead>
+         <tbody>
+          <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:bg-green-100">
+            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">Incasari</th>
+            <th className="text-green-700 text-xl">{ready ? result.cash : <p>awaiting</p>}</th>
+            <th>RON</th>
+          </tr>
+          <tr className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-green-100">
+            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">Fel Principal</th>
+            <th className="text-green-700 text-xl">{ready ? result.dishes : <p>awaiting</p>}</th>
+            <th>portii</th>
+          </tr>
+          <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:bg-green-100">
+            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">Supe/Ciorbe</th>
+            <th className="text-green-700 text-xl">{ready ? result.soup : <p>awaiting</p>}</th>
+            <th>portii</th>
+          </tr>
+          <tr className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-green-100">
+            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">Garnituri</th>
+            <th className="text-green-700 text-xl">{ready ? result.garnish : <p>awaiting</p>}</th>
+            <th>portii</th>
+          </tr>
+          <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:bg-green-100">
+            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">Salate</th>
+            <th className="text-green-700 text-xl">{ready ? result.salad : <p>awaiting</p>}</th>
+            <th>portii</th>
+          </tr>
+          <tr className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-green-100">
+            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">Desert</th>
+            <th className="text-green-700 text-xl">{ready ? result.desert : <p>awaiting</p>}</th>
+            <th>portii</th>
+          </tr>
+          <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:bg-green-100">
+            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">Chifle</th>
+            <th className="text-green-700 text-xl">{ready ? result.bread : <p>awaiting</p>}</th>
+            <th>bucati</th>
+          </tr>
+         </tbody>
+       </table>
+       </div>
         
       </div>
     </div>
